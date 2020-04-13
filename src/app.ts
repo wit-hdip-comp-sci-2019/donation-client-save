cc715a86676f84a8bc882f65bd15b894baad2b57
Eamonn de Leastar <edeleastar@wit.ie>
2020-04-13 16:35:50 +0100

    candidate view component introduced

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,1 @@
-import { Candidate } from "./services/donation-types";
-
-export class App {
-  firstName: string;
-  lastName: string;
-  office: string;
-  candidates: Candidate[] = [];
-
-  addCandidate() {
-    const candidate = {
-      firstName: this.firstName,
-      lastName: this.lastName,
-      office: this.office,
-    };
-    this.candidates.push(candidate);
-    console.log(candidate);
-  }
-}
+export class App {}
