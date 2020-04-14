780054b4f1aaf1e4f08638d2d2b6b285eef13a4f
Eamonn de Leastar <edeleastar@wit.ie>
2020-04-14 12:26:33 +0100

    bind donate to donation-service

--- src/views/donate.ts
+++ src/views/donate.ts
@@ -1,6 +1,14 @@
-import { Donation } from '../services/donation-types';
+import { inject } from 'aurelia-framework';
+import { Candidate, Donation } from '../services/donation-types';
+import { DonationService } from '../services/donation-service';
 
+@inject(DonationService)
 export class Donate {
   donations: Donation[] = [];
   paymentMethods = ['Cash', 'Paypal'];
+  candidates: Candidate[];
+
+  constructor(private ds: DonationService) {
+    this.candidates = ds.candidates;
+  }
 }
